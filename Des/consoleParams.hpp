a9dcba4c7c2036efc2eb459c885f3a2e7b4337a7
Loupehope <31570429+Loupehope@users.noreply.github.com>
2018-12-27 03:08:28 +0300

    Update consoleParams.hpp

--- Des/consoleParams.hpp
+++ Des/consoleParams.hpp
@@ -15,7 +15,7 @@
 class params {
     int count;
     char ** args;
-    
+
     /*
      * Проверка корректности введеного числа
      * @params i - номер аргумента
@@ -23,33 +23,31 @@ class params {
      * @params high - верхняя граница
      */
     bool isRightNumber(int i, char bottom, char high) const;
-    
+
     /*
      * Проверка существования файла
      * @params fname - путь файла
      */
     bool isFileExist() const;
-    
+
 public:
     /*
      * Конструктор
      */
     params(int count, char ** args) : count(count), args(args) {}
-    
+
     /*
      * Проверяет правильность введенных параметров
      */
     bool isParamsCorrect();
-    
+
     /*
      * Возвращает строку из параметров
      */
-    std::string getStringParam() const { return (std::string)args[2]; };
-    
+    std::string getStringParam() { return (std::string)args[2]; };
+
     /*
      * Возвращает число из параметров
      */
-    int getNumberParam() const;
+    int getNumberParam();
 };
-
-
