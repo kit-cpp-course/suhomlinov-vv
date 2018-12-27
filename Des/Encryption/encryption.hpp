af9ab40bc66197bb7c41761a69664e6a797f9a9e
Loupehope <31570429+Loupehope@users.noreply.github.com>
2018-12-27 03:05:24 +0300

    Update and rename encryptionDES.hpp to encryption.hpp

--- Des/Encryption/encryption.hpp
+++ Des/Encryption/encryption.hpp
@@ -8,41 +8,27 @@
 
 #pragma once
 #include "stdafx.hpp"
-#include "KeysGenerator.hpp"
-#include "Encryption.hpp"
+#include "Contents.hpp"
+#include "Keys.hpp"
 
 /*
  * Класс шифрования и дешифрования блока бит
  */
 
-class encryptionDES: public Encryption {
-    KeysGenerator & keyGen; // ссылка на объект, через который можно получить необходимые ключи
+class encryption: public Contents {
+    Keys & key; // ссылка на объект, через который можно получить необходимые ключи
     int mode; // режим: шифрование 1, дешифрование 0
-    
-    /*
-     * Шифрует данные
-     * @params text - незашифрованный текст
-     */
-    char * encrypt(char * text);
-    
-    /*
-     * Дешифрует данные
-     * @params text - зашифрованный текст
-     */
-    char * decrypt(char * text);
-    
 public:
     /*
      * Конструктор
      * @params key - ссылка на объект абстрактного класса Ключ
      * @params mode - режим (шифрование/дешифрование)
      */
-    encryptionDES(KeysGenerator & keyGen, const int & mode) : keyGen(keyGen), mode(mode) {}
-    
+    encryption(Keys & key, const int & mode) : key(key), mode(mode) {}
+
     /*
      * Получает блок битов (в виде массива bool) и возвращает (де)зашифрованный массив данных
      */
-    char * genereteContent(char *);
+    bool * genereteContent(bool *);
 };
-
 
