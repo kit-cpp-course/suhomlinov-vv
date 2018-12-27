8cad44db5bf83c67003eaad36603000066b21ee2
Loupehope <31570429+Loupehope@users.noreply.github.com>
2018-12-27 03:08:54 +0300

    Update fileEditor.cpp

--- Des/fileEditor.cpp
+++ Des/fileEditor.cpp
@@ -25,12 +25,28 @@ const string decrypt = "Dec";
   */
 int getFileSize(const string & pathFile);
 
+/*
+ * Переводит масссив символов в биты (bool)
+ * @params symbols - массив символов
+ * @params size - число битов
+ */
+bool * convertToBits(const char * const symbols, int size);
+
+/*
+ * Переводит масссив битов (bool) в символы (char)
+ * @params bites - массив битов
+ * @params size - число битов
+ */
+char * convertToChar(const bool * const bites, int size);
+
+char * getEncryptedSymbols(Contents & content, char * readSymbols);
+
 // Реализация функций
 
 void editor::reading(int length) {
     read = new char[blockLength];
     rFile.read(read, blockLength);
-    
+
     if (size >= length) { size -= blockLength; }
     else if (size < length && size) {
         for (int i = size; i < blockLength; i++) { read[i] = 0; } // В случае нечетного числа битов записываем в файл нули
@@ -52,8 +68,21 @@ void editor::getNewPath(const string path) {
 }
 
 void editor::writing(Contents & content) {
-    writeInFile(content.genereteContent(read), wFile);
+    char * symbols;
+
+    symbols = getEncryptedSymbols(content, read);
+    writeInFile(symbols, wFile);
+
     delete [] read;
+}
+
+char * getEncryptedSymbols(Contents & content, char * readSymbols) {
+    bool * bits, * cryptbits;
+
+    bits = convertToBits(readSymbols, block);
+    cryptbits = content.genereteContent(bits);
+
+    return convertToChar(cryptbits, block);;
 }
 
 void editor::writeInFile(const char * const symbols, ofstream & f) {
@@ -64,10 +93,22 @@ void editor::writeInFile(const char * const symbols, ofstream & f) {
 
 bool editor::isEnd() { return size; }
 
+bool * convertToBits(const char * const symbols, int size) {
+    bool * bits = new bool[size];
+    for (int i = 0; i < size; i++)
+        bits[i] = (symbols[i / 8] >> (i % 8)) & 1;
+    return bits;
+}
+
+char * convertToChar(const bool *  const bites, int size) {
+    char * letters = new char[size / 8];
+    memset(letters, 0, (size + 7) / 8);
+
+    for (int i = 0; i < size; i++) letters[i/8] |= bites[i] << (i % 8);
+    return letters;
+}
+
 int getFileSize(const string & pathFile) {
     fstream file(pathFile, ios::in|ios::ate);
     return (int)file.tellg();
 }
-
-
-
